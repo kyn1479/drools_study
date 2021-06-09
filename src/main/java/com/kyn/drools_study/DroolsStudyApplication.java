613d639e2dbee05d86f367b1780ac5190fd9764c
kyn1479 <1002289134@qq.com>
2021-06-09 11:07:05 +0800

    init project

---
+++ src/main/java/com/kyn/drools_study/DroolsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.kyn.drools_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroolsStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kyn/drools_study/DroolsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyn.drools_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
